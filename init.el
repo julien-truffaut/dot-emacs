cc120e310f2197df0e5b6bc4ddee2fd9ca593270
Julien Truffaut <truffaut.julien@gmail.com>
2016-04-27 19:00:18 +0100

    add haskell-mode config and gitignore

--- init.el
+++ init.el
@@ -1,5 +1,26 @@
-; Add MELPA repository
 (require 'package)
+(require 'req-package)
+
+; Add MELPA repository
 (add-to-list 'package-archives
   '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
+
+;; Haskell
+(req-package haskell-mode
+  :config (progn
+    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+    (add-hook 'haskell-mode-hook 'haskell-doc-mode)
+    (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+    (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
+    (add-hook 'haskell-mode-hook (lambda ()
+				   (electric-indent-local-mode -1)))
+    (setq haskell-process-type 'stack-ghci)
+    (setq haskell-process-path-ghci "stack")
+    (setq haskell-process-args-ghci '("ghci"))
+
+    (setq haskell-process-suggest-remove-import-lines t)
+    (setq haskell-process-auto-import-loaded-modules t)
+    (setq haskell-process-log nil)
+    (setq haskell-stylish-on-save t)))
